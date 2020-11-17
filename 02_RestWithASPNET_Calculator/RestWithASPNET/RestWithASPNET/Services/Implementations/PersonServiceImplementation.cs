845d0ac1ce1bae7615fcddc8c2ca28f6fe2e9d5e
Ronaldo Silva <ronaldojssilva@gmail.com>
2020-11-17 15:17:55 -0500

    Adding Person Service

---
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,68 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Ronaldo",
+                LastName = "Silva",
+                Address = "Quebec - Quebec - Canada",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
