83e9f01461157dc87c530c2d723ff9a4f44214de
Ronaldo Silva <ronaldojssilva@gmail.com>
2020-11-30 15:06:34 -0500

    Executando as primeiras migrations

---
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'sao paulo', 'airton', 'male', 'senna'),
+	(2, 'Anchiano - Itally', 'Leonardo', 'male', 'da Vinci'),
+	(3, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(4, 'Kentucky - USA', 'Mohamed Ali', 'Male', 'Gandhi'),
+	(5, 'Mizevo - South Africa', 'Nelson', 'Male', 'Mandela');
